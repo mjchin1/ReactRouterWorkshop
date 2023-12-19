1ec0e7ff43f7c3391c5326ac03277645cc8f2dd1
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-12-19 12:08:12 -0600

    adding updated files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+
+export default function NavBar() {
+    return (
+        <div id="navbar">
+            <Link to='/blue'>Blue</Link>
+            <Link to= '/red'>Red</Link>
+            <Link to= '/'>Home</Link>
+        </div>
+    );
+  }
